ad25d4b449ebba999b81820326ae6154f428acab
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-01-28 14:12:59 -0800

    add playerSelection from prompt, case insensitive

--- scripts.js
+++ scripts.js
@@ -11,8 +11,14 @@ function getComputerChoice() {
      }
 }
 console.log(getComputerChoice());  
+
 //Get playerSelection from a prompt
+let playerInput = prompt("Choose rock, paper, or scissors", "");
+
 //Make playerSelection case insensitive (change to lowercase)
+let playerSelection = playerInput.toLowerCase();
+console.log(playerSelection);
+
 //Compare playerSelection to computerSelection
 //Declare results of the playRound (win or loss) for whatever combination occurred
 //Record win or loss for the player for the round
