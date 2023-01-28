51ab2922dc6f17d9e73e7ef20203e90cc7301641
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-01-27 20:08:37 -0800

    add scripts comment build

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//getComputerChoice to randomly return rock, paper, or scissors as computerSelection
+//Get playerSelection from a prompt
+//Make playerSelection case insensitive (change to lowercase)
+//Compare playerSelection to computerSelection
+//Declare results of the playRound (win or loss) for whatever combination occurred
+//Record win or loss for the player for the round
+//Play game in 5 playRound
+//Display the results of each playRound
+//Declare player as a winner or loser of the game at the end of 5 playRound
