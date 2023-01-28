24856dd03d305a4b005dcab2e575ed2be7fa51d5
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-01-28 14:52:35 -0800

    add win-loss evaluation for playRound

--- scripts.js
+++ scripts.js
@@ -21,6 +21,25 @@ console.log(playerSelection);
 
 //Compare playerSelection to computerSelection
 //Declare results of the playRound (win or loss) for whatever combination occurred
+
+if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+} else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("You lose! Paper covers rock.");
+} else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("You win! Rock crushes scissors.");
+} else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("You lose! Scissors cut paper.");
+} else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("You win! Paper covers rock.");
+} else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("You lose! Rock crushes scissors.");
+} else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("You win! Scissors cut paper.");
+} else {
+    console.log("Hmm...are you sure you picked rock, paper, or scissors?");
+}
+
 //Record win or loss for the player for the round
 //Play game in 5 playRound
 //Display the results of each playRound
