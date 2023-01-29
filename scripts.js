62af84261226151dfd7c0acd00bda8e479d15574
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-01-29 10:54:56 -0800

    style playRound as function

--- scripts.js
+++ scripts.js
@@ -25,30 +25,34 @@ while ((wins < 3) && (losses < 3)) {
 
 //Compare playerSelection to computerSelection
 //Declare results of the playRound (win or loss) for whatever combination occurred
-//Record win or loss for the player for the round
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a tie!");
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         console.log("You lose! Paper covers rock.");
-        losses++;
+        return roundResult = "loss";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         console.log("You win! Rock crushes scissors.");
-        wins++;
+        return roundResult = "win";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         console.log("You lose! Scissors cut paper.");
-        losses++;
+        return roundResult = "loss";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("You win! Paper covers rock.");
-        wins++;
+        return roundResult = "win";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         console.log("You lose! Rock crushes scissors.");
-        losses++;
+        return roundResult = "loss";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         console.log("You win! Scissors cut paper.");
-        wins++;
+        return roundResult = "win";
     } else {
         console.log("Hmm...are you sure you picked rock, paper, or scissors?");
     }
+}
+
+//Record win or loss for the player for the round
+
 
 //Display the results of each playRound
     console.log(wins + " wins");
